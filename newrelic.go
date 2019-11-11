b4431cf02844a5cbb62379da575dcea0faa7c9a4
Daniel Fitzgerald <danielfitzgerald@newrelic.com>
2019-11-11 12:03:54 +0000

    initial commit

---
+++ newrelic.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// TODO: Read the URI and the license key for environment variables - DONE
+// TODO: Start a timer and flush events after timer ends
+// TODO: When the counter hits a batch size i.e.  200 or the timer hits flush time - send to New Relic
+// TODO: Read events and send events on seperate threads?
+// TODO: Check Logs buffer size - API will reject payloads over 1MB
+// TODO: Write to a log file for INFO and DEBUG messages
+
+func init() {
+	// setup logging
+	log.SetOutput(os.Stdout)
+}
+
+// how many logs will we hold in the buffer before transmitting?
+const bufferSize = 1
+
+// how long in ms before we transmit the logs if the buffer doesn't fill up first
+const flushMillis = 2000
+
+// LogPayload - a struct to hold the payload for an individual log message
+type LogPayload struct {
+	Logs [bufferSize]json.RawMessage `json:"logs"`
+}
+
+func readEnvironmentVariable(key string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		log.Error(key + " environment variable is not set!")
+		panic(key + " environment variable is not set!")
+	} else {
+		return value
+	}
+}
+
+func main() {
+	// a counter to keep track of how many logs in the buffer
+	var logCounter int
+	// an array to store the JSON strings we are sent by NXLog om_exec module
+	var logBuffer [bufferSize]json.RawMessage
+
+	// reader for stdin
+	reader := bufio.NewReader(os.Stdin)
+	// read a string from the reader delimited by newline char
+	message, _ := reader.ReadString('\n')
+	// log the message for debugging purposes
+	log.Info(message)
+	rawIn := json.RawMessage(message)
+
+	// store the JSON string, log, in the logs array
+	logBuffer[logCounter] = rawIn
+
+	// increment the log counter
+	logCounter++
+	log.Info("incremented the counter to: " + string(logCounter))
+
+	if logCounter == bufferSize {
+		var lp = LogPayload{Logs: logBuffer}
+		send(lp)
+	}
+}
+
+func send(logStruct LogPayload) {
+	log.Info("sending payload")
+	log.Info(logStruct.Logs)
+	arr, err := json.Marshal(logStruct)
+	if err != nil {
+		log.Error(err)
+	} else {
+		log.Info("parsed JSON: " + string(arr))
+	}
+
+	// Read API_URI and LICENSE_KEY from env vars
+	logAPIURI := readEnvironmentVariable("NEW_RELIC_LOG_URI")
+	licenseKey := readEnvironmentVariable("NEW_RELIC_LICENSE_KEY")
+
+	req, err := http.NewRequest("POST", logAPIURI, bytes.NewBuffer(arr))
+	if err != nil {
+		log.Error(err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("X-License-Key", licenseKey)
+
+	client := &http.Client{}
+	resp, httpErr := client.Do(req)
+	if httpErr != nil {
+		log.Info("There was an HTTP error", httpErr)
+		log.Fatal(httpErr)
+	} else {
+		log.Info("HTTP response code: " + string(resp.StatusCode))
+	}
+}
